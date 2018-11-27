52f8c0d7a53a17a1aad774a4518670ef4696bd77
Ying Dong <dongyingname@yahoo.com>
2018-11-26 21:23:11 -0700

    MessageList.jsx: Set Hardcoded Messages in the state and ready to render the messages

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
+import Message from "./Message.jsx";
 
+const printMessages = () => {
+  this.state.messages.map(message => {
+    let type = message.type;
+    let content = message.content;
+    let username = message.username;
+    return <Message />;
+  });
+};
 class MessageList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [
+        {
+          type: "incomingMessage",
+          content:
+            "I won't be impressed with technology until I can download food.",
+          username: "Anonymous1"
+        },
+        {
+          type: "incomingNotification",
+          content: "Anonymous1 changed their name to nomnom"
+        },
+        {
+          type: "incomingMessage",
+          content:
+            "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+          username: "Anonymous2"
+        },
+        {
+          type: "incomingMessage",
+          content: "...",
+          username: "nomnom"
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-        <main className="messages">
-        <div className="message">
-          <span className="message-username">Anonymous1</span>
-          <span className="message-content">
-            I won't be impressed with technology until I can download food.
-          </span>
-        </div>
-        <div className="message system">
-          Anonymous1 changed their name to nomnom.
-        </div>
+      <main className="messages">
+        <Message />
       </main>
     );
   }
