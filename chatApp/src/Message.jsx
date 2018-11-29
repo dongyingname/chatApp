88b2a7633a937af6f1736ee0bca1b0383dd9d337
Ying Dong <dongyingname@yahoo.com>
2018-11-29 13:04:49 -0700

    Message.jsx: Beautify. home.scss: make notification color red.

--- chatApp/src/Message.jsx
+++ chatApp/src/Message.jsx
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 class Message extends Component {
   render() {
-    // const type=this.props.message.type;
-    const content = this.props.message.content;
-    const username = this.props.message.username;
-    const notification = this.props.message.notification;
-    const nameColor =this.props.message.nameColor;
-    const imageURL = this.props.message.imageURL;
+    const {
+      content,
+      username,
+      notification,
+      nameColor,
+      imageURL
+    } = this.props.message;
     const styles = {
-      color:nameColor
+      color: nameColor
     };
     return (
       <li>
         <div className="message">
-          <span
-            className="message-username"
-            style={styles}
-          >
+          <span className="message-username" style={styles}>
             {username}
           </span>
           <span className="message-content">{content}</span>
